fcb863276805c0070ce878e1796976611667a5b1
Dennis Padiernos <depadiernos@gmail.com>
2020-01-23 21:15:03 -0600

    write model tests

--- models/items-model.spec.js
+++ models/items-model.spec.js
@@ -1,12 +1,27 @@
-const db = require("./data/db-config")
+const db = require("../data/db-config")
 const itemsModel = require("./items-model")
 
 beforeEach(async () => {
   await db.seed.run()
 })
 
-describe("items model", ()=>{
-  test("list", async ()=>{
-    const res = await ite
+describe("items model", () => {
+  test("list", async () => {
+    const res = await itemsModel.find()
+    expect(res.length).toBeGreaterThan(0)
+  })
+  test("findById", async () => {
+    const res = await itemsModel.findById(1)
+    expect(res.name).toBe("Pencil")
+  })
+  test("add", async () => {
+    const res = await itemsModel.add({name: "Book"})
+    expect(res.name).toBe("Book")
+  })
+  test("remove", async () => {
+    const before = await itemsModel.list()
+    await itemsModel.remove(1)
+    const after = await itemsModel.list()
+    expect(after.length).toBe(before.length)
   })
 })
